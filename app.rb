ad8d373065e9c521ef9bde9f2c82512e3e8d55b5
Eric Kim <hyeokjungkim@gmail.com>
2018-04-02 17:18:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @correct_names = {"name" => "Team Name",
+      "coach" => "Coach",
+      "pg" => "Point Guard",
+      "sg" => "Shooting Guard",
+      "sf" => "Shooting Forward",
+      "pf" => "Power Forward",
+      "c" => "Center"
+    }
+    @params = params.map {|k, v| [@correct_names[k], v] }.to_h
+    erb :team
+  end
 
 end
