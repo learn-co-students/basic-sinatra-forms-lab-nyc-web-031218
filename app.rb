efe1639ff9201a5d25546da7cfb03cfb95ff39fa
Eric Kim <hyeokjungkim@gmail.com>
2018-04-02 17:34:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,15 +7,22 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @correct_names = {"name" => "Team Name",
-      "coach" => "Coach",
-      "pg" => "Point Guard",
-      "sg" => "Shooting Guard",
-      "sf" => "Shooting Forward",
-      "pf" => "Power Forward",
-      "c" => "Center"
-    }
-    @params = params.map {|k, v| [@correct_names[k], v] }.to_h
+    # @correct_names = {"name" => "Team Name",
+    #   "coach" => "Coach",
+    #   "pg" => "Point Guard",
+    #   "sg" => "Shooting Guard",
+    #   "sf" => "Shooting Forward",
+    #   "pf" => "Power Forward",
+    #   "c" => "Center"
+    # }
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+    # @params = params.map {|k, v| [@correct_names[k], v] }.to_h
     erb :team
   end
 
