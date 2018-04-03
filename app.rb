ad9500565f344764cd6abb55dd14ef5ff6aef4dd
Rani Shah <rani.shah@flatironschool.com>
2018-04-03 08:37:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+   @team_name = params[:team_name]
+   @coach= params[:coach]
+   @shooting_guard = params[:shooting_guard]
+   @small_forward = params[:small_forward]
+   @power_forward = params[:power_forward]
+   @center = params[:center]
+   erb :team
+  end
 
 end
